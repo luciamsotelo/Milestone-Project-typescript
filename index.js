ae2f53aae8a1409e394d234e44f8680c0e59a659
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2023-10-21 21:59:46 -0700

    added timer and moves button

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
-const cardBoard = document.querySelector(".cards");
-const cardType = ["red","orange","yellow","green","blue","purple","lime","gold","pink","aqua"];
+// here are all of my constants, could not get the images 
+
+const cardType = ["red","orange","yellow","green","blue","purple","grey","brown","deeppink","aqua"];
 const cardSet = cardType.concat(cardType);
 const randomCard = cardSet.length;
 
+const gameBoard = document.querySelector(".cards");
+const startButton = document.getElementById("startButton")
+const currentAttempts =document.getElementById("attempts");
+
+// variables needed 
 let visibleCards = 0;
 let activeCard = null;
 let awaitingEndOfMove = false;
 let attempts = 0;
-let currentAttempts =document.getElementById("attempts");
-// attempts++;
-// currentAttempts.innerText = attempts;
+let timer;
+let seconds = 0;
 
+
+startButton.addEventListener("click", startGame);
+
+function startTimer() {
+    timer = setInterval(updateTimer, 1000);
+}
+function updateTimer() {
+    seconds++;
+    const minutes = Math.floor(seconds / 60).toString().padStart(2, "0");
+    const remainingSeconds = (seconds % 60).toString().padStart(2, "0");
+    const timerDisplay = document.getElementById("timer");
+    timerDisplay.innerText = `${minutes}:${remainingSeconds}`;
+}
+
+function startGame(){ 
+
+    gameBoard.innerHTML="";
+    startTimer();
+    // this makes the cards randomly appear
+    for (let i = 0; i < randomCard; i++) {
+        const randomIndex = Math.floor(Math.random() * cardSet.length);
+        const randomCard = cardSet[randomIndex];
+        const card = createCard(randomCard);
+    
+        cardSet.splice(randomIndex, 1);
+        gameBoard.appendChild(card);
+    }
+}
+
+function checkGameOver() {    
+    if (visibleCards === randomCard) {
+        stopTimer();
+        clearInterval(timer);
+    }
+}    
+function stopTimer() {
+    clearInterval(timer);
+}
+
+// this is where the cards are being revealed and hidden
 function createCard(randomCard) {
     const card = document.createElement("div");
-
     card.classList.add("card");
     card.setAttribute("data-cardType", randomCard);
     card.setAttribute("data-revealed", "false");
-
-    card.addEventListener("click", () => {
+//  here is the event listener to make it click and reveal and stay if match occurs and turns black if does not match
+    
+card.addEventListener("click", () => {
         const cardRevealed = card.getAttribute("data-revealed");
-
         if (awaitingEndOfMove || cardRevealed === "true" || card == activeCard) {
             return;
         }
-
         card.style.backgroundColor = randomCard;
 
         if (!activeCard) {
@@ -43,19 +86,21 @@ function createCard(randomCard) {
             awaitingEndOfMove = false;
             visibleCards += 2;
 
-            // if (visibleCards === randomCard) {
-            //     alert ("Congrats! Play Again");
-            // }
+            checkGameOver();
+
             return;
         }
+    //     return;
+    // }
         awaitingEndOfMove = true;
-            
+
         setTimeout(() => {
             card.style.backgroundColor = null;
             activeCard.style.backgroundColor = null;
         
             awaitingEndOfMove = false;
             activeCard = null;
+            updateAttempts();
         }, 1000);
         attempts++;
         currentAttempts.innerText = attempts;
@@ -64,16 +109,7 @@ function createCard(randomCard) {
     return card;
 }
 
-for (let i = 0; i < randomCard; i++) {
-    const randomIndex = Math.floor(Math.random() * cardSet.length);
-    const randomCard = cardSet[randomIndex];
-    const card = createCard(randomCard);
 
-    cardSet.splice(randomIndex, 1);
-    cardBoard.appendChild(card);
-
-    console.log(randomCard);
-}
 
 
 
